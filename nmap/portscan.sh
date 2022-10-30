7b0590dd14becbd5a09ca3fab851be9597465b30
Dexter Flodström <110725859+Dexter8242@users.noreply.github.com>
2022-10-30 17:19:51 +0000

    Update and rename portscan to nmap/portscan.sh

--- nmap/portscan.sh
+++ nmap/portscan.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Portscan script that grabs quickly grabs open ports,
-# then scans each open port in detail automatically.
+# Portscan script that grabs quickly grabs open ports, then scans each open port in detail automatically.
 findports=$(nmap -p- --host-timeout 201 --max-retries 0 --min-rate=1000 -T4 $1 | grep '^[0-9]*/tcp' | cut -d "/" -f1 | tr "\n" "," | sed s/.$//)
 nmap -T4 -A -p $findports $1 -oN open-ports
